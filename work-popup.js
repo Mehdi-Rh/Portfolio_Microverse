475b3464c820b601f8b7697fbd5c18e7494de90f
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-01-28 19:49:56 +0100

    Fix the first description issue (mobile version)

--- work-popup.js
+++ work-popup.js
@@ -2,7 +2,7 @@ const projects = [
   {
     title: 'Tonic',
     src: 'images/works/Snapshoot-Portfolio-1.png',
-    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."],
     technologies: ['html', 'Ruby on rails', 'css', 'Javascript'],
     seeLiveLink: '#',
     seeSourceLink: '#',
