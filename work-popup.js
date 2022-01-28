eaf70475cba97f02c4b51ad066c41513706283bf
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-01-28 17:45:40 +0100

    Finish the popup window

--- work-popup.js
+++ work-popup.js
@@ -2,7 +2,7 @@ const projects = [
   {
     title: 'Tonic',
     src: 'images/works/Snapshoot-Portfolio-1.png',
-    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."],
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
     technologies: ['html', 'Ruby on rails', 'css', 'Javascript'],
     seeLiveLink: '#',
     seeSourceLink: '#',
@@ -126,10 +126,7 @@ function popUpMobile(cardNbr) {
 
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener('click', () => {
-    popupDesk(i);
-
-  });
-  buttons[i].addEventListener('click', () => {
-    popUpMobile(i);
+    window.innerWidth >= 992 ? popupDesk(i): popUpMobile(i);
+    document.body.style.overflow = "hidden";
   });
 }
