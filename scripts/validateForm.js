bdae9540e635b305c81f2b118563ead2aca0ef15
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-02-03 10:47:30 +0100

    Fix linter errors

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -4,16 +4,13 @@ const form = document.getElementById('contacts');
 const errorElement = document.getElementById('error');
 
 form.addEventListener('submit', (event) => {
-  let messages = [];
-  if (email.value !== email.value.toLowerCase()) {
-    messages.push('The email is not valid, email charachters should be lowercase');
-  }
+  const messages = ['The email is not valid, email charachters should be lowercase'];
 
-  if (messages.length > 0) {
+  if (email.value !== email.value.toLowerCase()) {
     event.preventDefault();
     errorElement.innerText = messages.join(', ');
     errorElement.style.display = ('flex');
   } else {
     errorElement.style.display = ('none');
   }
-})
+});
